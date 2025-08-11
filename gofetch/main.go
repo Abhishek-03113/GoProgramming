7ebf785aba59235fdacf1a7f37fd01d9b55c0e80
Abhishek-03113 <abhishekpawar2021.ainds@mmcoe.edu.in>
2025-08-11 12:08:36 +0530

    completed the implementation

--- gofetch/main.go
+++ gofetch/main.go
@@ -12,13 +12,62 @@ import (
 )
 
 func main() {
-	getMemoryStats()
+
+	// hostname
+	fmt.Println(getHostName())
+	// os
+	fmt.Println(getOS())
+	// host
+	fmt.Println(getHost())
+	// kernel
+	fmt.Println(getKernel())
+	// uptime
+	fmt.Println(getUptime())
+	// shell
+	fmt.Println(getShell())
+	// resolution
+	fmt.Println(getDisplayResolution())
+	// DE
+	fmt.Println(getDE())
+	// WM
+	fmt.Println(getWM())
+	// Terminal
+	fmt.Println(getTerm())
+	// CPU
+	fmt.Println(getCPU())
+	// GPU
+	fmt.Println(getGPU())
+	// Memory
+	fmt.Println(getMemoryStats())
+}
+
+func getWM() string {
+	wm := "Quartz Compositor" // default for macOS
+	candidates := []string{"Rectangle", "Amethyst", "yabai", "Magnet", "chunkwm"}
+
+	out, _ := exec.Command("ps", "-A").Output()
+	processList := string(out)
+
+	for _, c := range candidates {
+		if strings.Contains(processList, c) {
+			wm = c
+			break
+		}
+	}
+
+	return fmt.Sprintf("%s", wm)
+
 }
 
 func getHostName() string {
 	user := os.Getenv("USER")
 	hostname, _ := os.Hostname()
 	return fmt.Sprintf("%s@%s", user, hostname)
+}
+
+func getHost() string {
+	host, _ := os.Hostname()
+	return host
 }
 
 func getTerm() string {
@@ -33,7 +82,7 @@ func getOS() string {
 	versionstr := strings.Trim(string(version), "\n")
 	buildstr := strings.Trim(string(build), "\n")
 	archget, _ := exec.Command("uname", "-m").Output()
-	arch := string(archget)
+	arch := strings.Trim(string(archget), "\n")
 	return fmt.Sprintf("%s : %s (%s) %s", namestr, versionstr, buildstr, arch)
 }
 
@@ -52,21 +101,19 @@ func getCPU() string {
 	cpuCount := runtime.NumCPU()
 	cpuver, _ := exec.Command("sysctl", "-n", "machdep.cpu.brand_string").Output()
 	cpu := strings.Trim(string(cpuver), "\n")
-	return fmt.Sprintf("%s (%d) \n", cpu, cpuCount)
+	return fmt.Sprintf("%s (%d)", cpu, cpuCount)
 }
 
 func getMemoryStats() string {
 
 	pageSizeBytes, _ := exec.Command("sysctl", "-n", "hw.pagesize").Output()
 	memsizeBytes, _ := exec.Command("sysctl", "-n", "hw.memsize").Output()
-	inactivePagesBytes, _ := exec.Command("sysctl", "-n", "vm.page_reusable_count").Output()
 
 	out, err := exec.Command("vm_stat").Output()
 	if err != nil {
 		panic(err)
 	}
 
-	// Helper to extract page counts
 	parse := func(key string) uint64 {
 		re := regexp.MustCompile(key + ":[ \t]+([0-9]+)\\.")
 		match := re.FindStringSubmatch(string(out))
@@ -76,20 +123,51 @@ func getMemoryStats() string {
 		val, _ := strconv.ParseUint(match[1], 10, 64)
 		return val
 	}
-
 	activePages := parse("Pages active")
-
 	pageSizeStr := strings.Trim(string(pageSizeBytes), "\n")
 	memSizeStr := strings.Trim(string(memsizeBytes), "\n")
-	inactivePagesStr := strings.Trim(string(inactivePagesBytes), "\n")
-	fmt.Println(inactivePagesStr)
 
 	memfloat, _ := strconv.ParseInt(memSizeStr, 10, 64)
 	pageSize, _ := strconv.ParseInt(pageSizeStr, 10, 64)
 
-	memory := float64(memfloat) / (1024 * 1024 * 1024)
-	freeMemory := float64(memory-(float64(activePages)*float64(pageSize))) / (1024 * 1024 * 1024)
+	memory := float64(memfloat) / (1024 * 1024)
+	freeMemory := float64(memory - (float64(activePages)*float64(pageSize))/(1024*1024))
 
-	fmt.Println(memory, freeMemory)
-	return ""
+	return fmt.Sprintf("%.fMiB / %.fMiB", freeMemory, memory)
+}
+
+func getShell() string {
+	return os.Getenv("SHELL")
+}
+
+func getGPU() string {
+
+	out, _ := exec.Command("sh", "-c", `ioreg -r -d1 -c "IOPCIDevice" | grep "model"`).Output()
+
+	lines := strings.Split(string(out), "\n")
+	var gpu []interface{}
+	for _, line := range lines {
+		if strings.Contains(line, "model") {
+			name := strings.TrimSpace(strings.Split(line, "<\"")[1])
+			name = strings.TrimSuffix(name, "\">")
+			gpu = append(gpu, name)
+		}
+	}
+	return fmt.Sprintf("%s %s", gpu...)
+}
+
+func getKernel() string {
+	out, _ := exec.Command("uname", "-v").Output()
+	outStr := strings.Trim(strings.Split(string(out), ":")[0], "\n")
+
+	return fmt.Sprintf("%s", outStr)
+}
+
+func getDisplayResolution() string {
+	out, _ := exec.Command("sh", "-c", `system_profiler SPDisplaysDataType | grep "Resolution:" | head -n 1`).Output()
+	return fmt.Sprintf("%s", strings.Trim(strings.TrimSpace(string(out)), "\n"))
+}
+
+func getDE() string {
+	return fmt.Sprintf("Aqua")
 }
