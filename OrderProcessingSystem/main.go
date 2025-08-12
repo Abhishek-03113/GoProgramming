941bbe5aa18a35b87a2d580090c20f7840566173
Abhishek-03113 <abhishekpawar2021.ainds@mmcoe.edu.in>
2025-08-12 17:42:34 +0530

    added some code

--- OrderProcessingSystem/main.go
+++ OrderProcessingSystem/main.go
@@ -5,7 +5,10 @@ import (
 	"fmt"
 	"log"
 	"math/rand"
+	"sync"
 )
+
+var wg sync.WaitGroup
 
 type Order struct {
 	orderId int
@@ -13,34 +16,43 @@ type Order struct {
 	Amount  float64
 }
 
-func ProcessOrder(workerId int, orderChannel chan Order) {
+func ProcessOrder(workerId int, orderChannel chan Order, stockMap map[string][2]float64) {
+	defer wg.Done()
 	for order := range orderChannel {
 		if order.Amount == -1 {
 			fmt.Printf("[Worker-%d] Processing Order %d ....  Out of Stock", workerId, order.orderId)
-			continue
-		}
-		var paymentStatus string
-		_, err := getPayment()
-		if err != nil {
-			paymentStatus = "Payment Failed"
-			log.Println(err)
+
 		} else {
-			paymentStatus = "Payment Successful"
+			var paymentStatus string
+			_, err := getPayment()
+			if err != nil {
+				paymentStatus = "Payment Failed"
+				log.Println(err)
+			} else {
+				paymentStatus = "✅ Payment Success"
+				for _, item := range order.Items {
+					arr := stockMap[item]
+					arr[0]--
+					stockMap[item] = arr
+				}
+			}
+			fmt.Printf("[Worker-%d] Processing Order %d ....  %s \n", workerId, order.orderId, paymentStatus)
 		}
-		fmt.Printf("[Worker-%d] Processing Order %d ....  %s", workerId, order.orderId, paymentStatus)
 	}
+
+	//fmt.Printf("Closing the order channel\n")
 }
 
 func Checkout(items []string, stockMap map[string][2]float64) (float64, error) {
 	var bill float64
 	for _, item := range items {
-		if stockMap[item][0] < 0 {
+		if stockMap[item][0] <= 0 {
 			return -1, fmt.Errorf("item %s is out of stock", item)
 		}
 		bill += stockMap[item][1]
 	}
 
-	return bill, _
+	return bill, nil
 }
 
 func CreateOrder(orderID int, Items []string, stockMap map[string][2]float64) *Order {
@@ -61,7 +73,7 @@ func getPayment() (bool, error) {
 	if random == 0 {
 		return false, errors.New("payment failed")
 	}
-	return true, _
+	return true, nil
 }
 
 func main() {
@@ -73,5 +85,26 @@ func main() {
 	}
 
 	orderChannel := make(chan Order)
+
+	for i := range 10 {
+		wg.Add(1)
+		go ProcessOrder(i, orderChannel, stockMap)
+	}
+
+	order1 := CreateOrder(1, []string{"Phone", "Charger", "Mouse"}, stockMap)
+	order2 := CreateOrder(2, []string{"Laptop"}, stockMap)
+	order3 := CreateOrder(3, []string{"Phone", "Charger"}, stockMap)
+	order4 := CreateOrder(4, []string{"Mouse", "Laptop", "Mouse"}, stockMap)
+
+	orders := []Order{
+		*order3, *order2, *order1, *order4,
+	}
+
+	for i := 0; i < 4; i++ {
+		orderChannel <- orders[i]
+	}
+
+	close(orderChannel)
+	wg.Wait()
 
 }
