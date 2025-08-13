9ef52bb9a0d51202ee1985c6a9f62962f1a08d15
Abhishek-03113 <abhishekpawar2021.ainds@mmcoe.edu.in>
2025-08-13 15:20:19 +0530

    modified some stuff

--- OrderProcessingSystem/main.go
+++ OrderProcessingSystem/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 	"math/rand"
 	"sync"
@@ -9,11 +8,17 @@ import (
 
 var wg sync.WaitGroup
 var lock sync.RWMutex
+var errs []error
+var orderCounter int
 
 type Order struct {
 	orderId int
 	Items   []string
 	Amount  float64
+}
+
+func (o *Order) String() string {
+	return fmt.Sprintf("Order ID : %d\nOrder Items : %v\nOrder Amount : %.2f", o.orderId, o.Items, o.Amount)
 }
 
 type StockItem struct {
@@ -25,14 +30,16 @@ type StockItem struct {
 func ProcessOrder(workerId int, orderChannel chan *Order, stockMap map[string]*StockItem) {
 	defer wg.Done()
 	for order := range orderChannel {
-		amount, _ := Checkout(order.Items, stockMap)
+		amount, err := Checkout(order.Items, stockMap)
 		order.Amount = amount
-		if order.Amount == -1 {
+		if err != nil {
+			errs = append(errs, err)
 			fmt.Printf("[Worker-%d] Processing Order %d ....  ❌ Out of Stock\n", workerId, order.orderId)
 		} else {
 			var paymentStatus string
-			_, err := getPayment()
+			_, err := getPayment(order.Amount)
 			if err != nil {
+				errs = append(errs, err)
 				paymentStatus = "❌ Payment Failed"
 				fmt.Printf("[Worker-%d] Processing Order %d ....  %s\n", workerId, order.orderId, paymentStatus)
 			} else {
@@ -43,8 +50,8 @@ func ProcessOrder(workerId int, orderChannel chan *Order, stockMap map[string]*S
 						stockItem.quantity--
 					}
 				}
-				lock.Unlock()
 				fmt.Printf("[Worker-%d] Processing Order %d ....  %s\n", workerId, order.orderId, paymentStatus)
+				lock.Unlock()
 			}
 		}
 	}
@@ -64,17 +71,19 @@ func Checkout(items []string, stockMap map[string]*StockItem) (float64, error) {
 	return bill, nil
 }
 
-func CreateOrder(orderID int, items []string) *Order {
+func CreateOrder(orderID int, items []string, stockMap map[string]*StockItem) *Order {
+	amount, _ := Checkout(items, stockMap)
 	return &Order{
 		orderId: orderID,
 		Items:   items,
+		Amount:  amount,
 	}
 }
 
-func getPayment() (bool, error) {
-	random := rand.Int63n(4)
+func getPayment(amount float64) (bool, error) {
+	random := rand.Int63n(3)
 	if random == 0 {
-		return false, errors.New("payment failed")
+		return false, fmt.Errorf("payment of rs %.2f failed", amount)
 	}
 	return true, nil
 }
@@ -95,18 +104,26 @@ func main() {
 		go ProcessOrder(i, orderChannel, stockMap)
 	}
 
-	order1 := CreateOrder(1, []string{"phone", "charger", "mouse"})
-	order2 := CreateOrder(2, []string{"laptop"})
-	order3 := CreateOrder(3, []string{"phone", "charger"})
-	order4 := CreateOrder(4, []string{"mouse", "laptop", "mouse"})
+	order1 := CreateOrder(1, []string{"phone", "charger", "mouse"}, stockMap)
+	order2 := CreateOrder(2, []string{"laptop"}, stockMap)
+	order3 := CreateOrder(3, []string{"phone", "charger"}, stockMap)
+	order4 := CreateOrder(4, []string{"mouse", "laptop", "mouse"}, stockMap)
 
 	orders := []*Order{
 		order3, order2, order1, order4,
 	}
 
+	fmt.Println("-------Orders-------")
+	for _, order := range orders {
+		fmt.Println(order)
+		fmt.Println("-------------------")
+
+	}
+	fmt.Println()
 	for _, order := range orders {
 		orderChannel <- order
 	}
+
 	//scanner := bufio.NewScanner(os.Stdin)
 	//orderCounter := 1
 	//
@@ -132,11 +149,17 @@ func main() {
 	//	for _, item := range itemslist {
 	//		items = append(items, strings.TrimSpace(item))
 	//	}
-	//	order := CreateOrder(orderCounter, items)
+	//	order := CreateOrder(orderCounter, items, stockMap)
 	//	orderCounter++
 	//	orderChannel <- order
 	//}
+
 	close(orderChannel)
 	wg.Wait()
-	fmt.Println("All orders processed for today")
+
+	fmt.Println()
+	fmt.Println("-------Errors-------")
+	for _, err := range errs {
+		fmt.Printf("Error : %v \n", err)
+	}
 }
